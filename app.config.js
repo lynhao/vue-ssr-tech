2b4930bb99895cf46bc5d3f6c27ce36328ecb50c
linhao <linhao@foresee.com.cn>
2018-04-19 15:26:11 +0800

    single quote

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   db: {
-    appId: "A6078474646891",
-    appKey: "80FE4EF6-40AA-8C04-DA05-8C7D4AED9040"
+    appId: 'A6078474646891',
+    appKey: '80FE4EF6-40AA-8C04-DA05-8C7D4AED9040'
   }
 }
